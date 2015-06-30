35bb019f1a6e389fe26b38b171e1bbc76bd1c1ca
Fred Cons <cons@fullsix.com>
2015-06-30 11:40:06 +0200

    Building html and pdf

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+gitbook build
 # note : for this to work, one must have installed gitbook-cli and ebook-convert (through npm) and calibre (through apt-get)
 gitbook pdf . ./introduction_elasticsearch.pdf
